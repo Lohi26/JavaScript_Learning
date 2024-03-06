e3ff8c7423fe52f146c2f59678db3e3851920b68
Lohi26 <lohitachandru@gmail.com>
2024-03-06 12:33:23 +0530

    todays learning

---
+++ Practice_Problems/challenege7.js
@@ -0,0 +1,16 @@
+// Rewrite the 'calcAverageHumanAge' function from Challenge #2, but this time
+// as an arrow function, and using chaining!
+// Test data:
+// § Data 1: [5, 2, 4, 1, 15, 8, 3]
+// § Data 2: [16, 6, 10, 5, 6, 1, 4]
+
+
+const sumAverage=(julia.map( ele => ele<=2? ele*2 : 16+(ele*4)).filter(arr => arr>=18).reduce( (acc,arrayEle,i,arr) => acc+arrayEle/arr.length,0));
+console.log(sumAverage);
+
+
+const kateMax=kate.reduce( (acc,ar) => Math.max(acc,ar),kate[0]);
+console.log(kateMax);
+
+
+//In previous challenge i have given thea answer in the chainng method🤷‍♀️
