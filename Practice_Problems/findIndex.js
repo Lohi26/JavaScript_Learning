84aa3cc72aa215ae1ed7b9e3aaccab029624edd6
Lohi26 <lohitachandru@gmail.com>
2024-03-06 18:58:05 +0530

    learning daily

---
+++ Practice_Problems/findIndex.js
@@ -0,0 +1,43 @@
+const arr=[10,20,48,56,78,100];
+
+const numberIndex=arr.findIndex(ele => ele===48);
+console.log(numberIndex);
+
+
+//it returns the index of the satisfied element in the array
+
+
+const account1 = {
+    owner: "Lohitav Chandrasekaran",
+    amount: [200, 450, -400, 3000, -650, -130, 70, 1300],
+    interestRate: 1.2,
+    pin: 26
+};
+
+const account2 = {
+    owner: "Minnie Innocent",
+    amount: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
+    interestRate: 1.5,
+    pin: 19
+};
+
+const account3 = {
+    owner: "Sanjay Karthik",
+    amount: [200, -200, 340, -300, -20, 50, 400, -460,700],
+    interestRate: 0.7,
+    pin: 20
+};
+
+const account4 = {
+    owner: "Padmavathi Kannappan",
+    amount: [430, 1000, 700, 50, 90],
+    interestRate: 1,
+    pin: 6
+};
+const accounts=[account1,account2,account3,account4];
+
+const obj=accounts.findIndex(account => account.pin===6);
+console.log(obj);
+
+
+//return the objects index in the array
