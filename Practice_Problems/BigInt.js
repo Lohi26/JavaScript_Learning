fb73ba6e73c8aa7de73af17b7f358523c1c334e5
Lohi26 <lohitachandru@gmail.com>
2024-03-19 21:31:41 +0530

    remainderoperator and rounding of interger and decimal values

---
+++ Practice_Problems/BigInt.js
@@ -0,0 +1,12 @@
+console.log(Number.MAX_SAFE_INTEGER)
+
+console.log(BigInt(900719925474099132467))
+
+
+const a=12345679127825632465789764n;
+const b=10;
+console.log(a*BigInt(b));
+
+console.log(typeof a);
+
+console.log( typeof "hi"+ typeof b);
---
+++ Practice_Problems/NumberSeperator.js
@@ -0,0 +1,8 @@
+const num=10_00_000;
+console.log(num);
+
+//it eleimates the underscore 
+
+
+console.log(Number("10_00"));
+console.log(parseInt("10_00_000"));
