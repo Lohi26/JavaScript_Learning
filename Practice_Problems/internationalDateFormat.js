6e92ee10448d422d460b4c055638fad9f012fa5e
Lohi26 <lohitachandru@gmail.com>
2024-03-21 10:57:30 +0530

    dates and numbers international

---
+++ Practice_Problems/internationalDateFormat.js
@@ -0,0 +1,9 @@
+const date=new Date(2024,10,26);
+const inter=new Intl.DateTimeFormat("pt-PT").format(date);
+console.log(inter);
+
+//Examples of international time format
+//en-US
+//pt-PT
+//en-CA
+//en-AU
---
+++ Practice_Problems/internationalNumberFormat.js
@@ -0,0 +1,3 @@
+const num=1000000;
+const number=new Intl.NumberFormat("ar-SY").format(num);
+console.log(number);
